b67fc6408f2cd7751d9334d3d1fff84129029e7f
John Hopkins <johnthopkins@gmail.com>
2016-09-20 11:44:20 -0700

    set up jasmine tests, made sure it worked with concat function

---
+++ spec/concat_spec.js
@@ -0,0 +1,24 @@
+const concat = require('../src/concat.js')
+
+fdescribe('concat', ()=> {
+  it('returns a new array with the input values', () => {
+    expect(concat([1,2,3], 2)).toEqual([1,2,3,2])
+  })
+
+  it('returns a new array with the input values', () => {
+    expect(concat([1,2,3], null)).toEqual([1,2,3, null])
+  })
+
+  it('returns a new array with the input string', () => {
+    expect(concat('string')).toEqual(['string'])
+  })
+
+  it('returns a new array with the input object', () => {
+    expect(concat({a:1})).toEqual([{a:1}])
+  })
+
+  it('returns a new array with the input object', () => {
+    expect(concat([1,2,3,4,5], 6,7)).toEqual([1,2,3,4,5,6,7])
+  })
+
+})
