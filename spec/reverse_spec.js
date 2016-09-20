8195c8c61886d8f276811190be14b2adc51afe29
John Hopkins <johnthopkins@gmail.com>
2016-09-20 16:06:44 -0700

    reverse works

---
+++ spec/reverse_spec.js
@@ -0,0 +1,20 @@
+const reverse  = require('../src/reverse.js')
+
+fdescribe('reverse', ()=> {
+  it('returns an array with the values in reverse', () => {
+    expect(reverse([1,'b',3])).toEqual([ 3,'b',1])
+  })
+
+  it('returns an object as is', () => {
+    expect(reverse( {a: 'a', b: 'b'})).toEqual({a: 'a', b: 'b'})
+  })
+
+  it('returns null for a null input', () => {
+    expect(reverse(null)).toEqual(null)
+  })
+
+  it('returns undefined for an undefined input', () => {
+    expect(reverse(undefined)).toEqual(undefined)
+  })
+
+})
