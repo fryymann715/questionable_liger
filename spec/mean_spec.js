627e9b7774358bc65823d81c55008f4155aa797d
John Hopkins <johnthopkins@gmail.com>
2016-09-21 09:27:09 -0700

    cleanup logs & indentation

--- spec/mean_spec.js
+++ spec/mean_spec.js
@@ -1,16 +1,16 @@
 const mean=require('../src/mean.js')
 
-fdescribe('mean',()=>{
-it('returns the mean of a given array',()=>{
-  expect(mean([1,2,3,4])).toEqual(2.5)
-})
+describe('mean',()=>{
+  it('returns the mean of a given array',()=>{
+    expect(mean([1,2,3,4])).toEqual(2.5)
+  })
 
-it('returns the mean of a given array',()=>{
-  expect(mean([4])).toEqual(4)
-})
+  it('returns the mean of a given array',()=>{
+    expect(mean([4])).toEqual(4)
+  })
 
 
-it('returns the mean of a given array',()=>{
-  expect(mean([1,5])).toEqual(3)
-})
+  it('returns the mean of a given array',()=>{
+    expect(mean([1,5])).toEqual(3)
+  })
 })
--- spec/size_spec.js
+++ spec/size_spec.js
@@ -1,18 +1,22 @@
 const size = require('../src/size.js')
 
-fdescribe('size', ()=> {
+describe('size', ()=> {
 
-  it('Gets the size of a collection by returning its length', () => {
+  it('Gets the size of a string by returning its length', () => {
     expect(size('four')).toEqual(4)
   })
 
-  it('Gets the size of a collection by returning its length', () => {
+  it('Gets the size of an array by returning its # of elements', () => {
     expect(size([2, 5, 7, 901234, 'eight'])).toEqual(5)
   })
 
-  it('Gets the size of a collection by returning its length', () => {
+  it('Gets the size of an object by returning # of key value pairs', () => {
     expect(size({a:14, b:12})).toEqual(2)
   })
 
+  // Here's a new edge case
+  it('Testing operation on unusual data', () => {
+    expect(size(undefined)).toEqual(0)
+  })
 
 })
