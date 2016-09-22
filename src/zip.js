344033f78455bc047e9ad0262636699385e2424a
John Hopkins <johnthopkins@gmail.com>
2016-09-22 14:23:28 -0700

    zip works!

---
+++ src/zip.js
@@ -0,0 +1,39 @@
+const zip = (...arrays) => {
+  const result = []
+  const max = maxLength(arrays)
+
+  // for(let index = 1; index < arrays.length; index++){
+  //   if (arrays[index].length > max){
+  //     max = arrays[index].length
+  //   }
+  // }
+
+
+  for(let index = 0; index < max; index++){
+    console.log(max);
+    let temp = []
+
+
+      for(let arrayIndex = 0; arrayIndex < arrays.length; arrayIndex++){
+        if(arrays[arrayIndex] instanceof Array ){
+        temp.push(arrays[arrayIndex][index])
+        console.log(temp);
+      }
+    }
+      result.push(temp)
+  }
+  return result
+ }
+
+const maxLength = (arrays) =>{
+  let max = arrays[0].length || 0 
+  for(let index = 1; index < arrays.length; index++){
+    if (arrays[index].length > max){
+      console.log('max= ',max);
+      max = arrays[index].length
+    }
+  }
+  return max
+}
+
+module.exports = zip
