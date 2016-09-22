2967887f4239172759703ff903bbf5d8a421863b
Amelia Clarke <ameliavoncat@gmail.com>
2016-09-22 09:21:44 -0700

    stable-ish state for tests

--- src/difference.js
+++ src/difference.js
@@ -1,19 +1,16 @@
-const difference=(arr1,...values)=>{
-let differenceArray=[]
+const difference = (arr1, ...values) => {
+ let differenceArray=[]
 
-const matchCheck=(val,arr)=>{
-  for(let i=0;i<arr.length;i++){
-    if (val!==arr[i]){
-      if(!matchCheck(val, differenceArray){
-        
+  const matchCheck = (val,arr) => {
+    for( let i = 0; i < arr.length; i++ ) {
+      if( val !== arr[ i ] ) {
+        if( !matchCheck( val, differenceArray ) ){
+
+        }
       }
     }
   }
-}
 
 }
 
-
-
-
-module.exports=difference
+module.exports = difference
