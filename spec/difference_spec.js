1dec1c3051f188fd71ad35817211f3b439670ee7
Amelia Clarke <ameliavoncat@gmail.com>
2016-09-22 12:00:23 -0700

    difference function and tests

--- spec/difference_spec.js
+++ spec/difference_spec.js
@@ -1,11 +1,17 @@
 const difference=require('../src/difference.js')
 
-describe('difference', ()=>{
-  it('returns an array containing values not found in given arrays',()=>{
+fdescribe('difference', ()=>{
+  it('differentiates two number arrays',()=>{
     expect(difference([1,2,3],[3,4,5])).toEqual([1,2,4,5])
   })
-  it('returns an array containing values not found in given arrays',()=>{
+  it('differentiates two string arrays',()=>{
     expect(difference(["cat","dog","owl"],["cat","dog","parrot"])).toEqual(['owl','parrot'])
   })
+  it('checks an array for given values and adds them if not found',()=>{
+    expect(difference([1,2,3],4)).toEqual([1,2,3,4])
+  })
 
+  it('checks an array for given values and adds them if not found',()=>{
+    expect(difference([1,2,3],2,4)).toEqual([1,2,3,4])
+  })
 })
--- src/difference.js
+++ src/difference.js
@@ -1,16 +1,67 @@
 const difference = (arr1, ...values) => {
  let differenceArray=[]
 
-  const matchCheck = (val,arr) => {
-    for( let i = 0; i < arr.length; i++ ) {
-      if( val !== arr[ i ] ) {
-        if( !matchCheck( val, differenceArray ) ){
+
+   const matchCheck = (val,arr) => {
+     let match= false
+     for( let i = 0; i < arr.length; i++ ) {
+
+       if( val === arr[ i ] ) {
+         match = true
+         return match
+       }
+}
+return match
+}
+for(var i=0;i<values.length;i++){
+  if(values[i].constructor===Array){
+
+        for(let k=0;k<arr1.length;k++){
+          if(!matchCheck(arr1[k],values[i])){
+            if(!matchCheck(arr1[k],differenceArray)){
+              differenceArray.push(arr1[k])
+
+            }
+          }
 
         }
+
+    for(let j=0;j<values[i].length;j++){
+      if(!matchCheck(values[i][j],arr1)){
+        if(!matchCheck(values[i][j],differenceArray)){
+          differenceArray.push(values[i][j])
+
+        }
+      }
+
+    }
+
+  } else {
+
+
+          for(let k=0;k<arr1.length;k++){
+            if(!matchCheck(arr1[k],values[i])){
+              if(!matchCheck(arr1[k],differenceArray)){
+                differenceArray.push(arr1[k])
+
+              }
+            }
+
+          }
+    if(!matchCheck(values[i],arr1)){
+      if(!matchCheck(values[i],differenceArray)){
+        differenceArray.push(values[i])
+
       }
     }
   }
 
+}
+
+
+
+console.log('values are: '+values);
+return differenceArray
 }
 
 module.exports = difference
