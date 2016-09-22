667c52bd8c345f67331e38fffa230de816ed6c18
Amelia Clarke <ameliavoncat@gmail.com>
2016-09-22 09:08:45 -0700

    wip: difference and replace started

---
+++ spec/difference_spec.js
@@ -0,0 +1,11 @@
+const difference=require('../src/difference.js')
+
+fdescribe('difference', ()=>{
+  it('returns an array containing values not found in given arrays',()=>{
+    expect(difference([1,2,3],[3,4,5])).toEqual([1,2,4,5])
+  })
+  it('returns an array containing values not found in given arrays',()=>{
+    expect(difference(["cat","dog","owl"],["cat","dog","parrot"])).toEqual(['owl','parrot'])
+  })
+
+})
---
+++ src/difference.js
@@ -0,0 +1,19 @@
+const difference=(arr1,...values)=>{
+let differenceArray=[]
+
+const matchCheck=(val,arr)=>{
+  for(let i=0;i<arr.length;i++){
+    if (val!==arr[i]){
+      if(!matchCheck(val, differenceArray){
+        
+      }
+    }
+  }
+}
+
+}
+
+
+
+
+module.exports=difference
