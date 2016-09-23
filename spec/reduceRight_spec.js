b5710979197076ecdc6439da1e95070629fb885f
Amelia Clarke <ameliavoncat@gmail.com>
2016-09-23 11:13:21 -0700

    fixed tests to be in es6

--- spec/reduceRight_spec.js
+++ spec/reduceRight_spec.js
@@ -2,13 +2,13 @@ const reduceRight = require('../src/reduceRight.js')
 
 describe('reduceRight',()=> {
   it('reduces a collection to a value by running each through a function and returning the result to the next run of the function',()=>{
-    expect(reduceRight([1,2],function(a,b){console.log('a is ' +a); console.log('b is ' +b); return a+b})).toEqual(3)
+    expect(reduceRight([1,2],(a,b)=>{return a+b})).toEqual(3)
   })
   it('reduces a collection to a value by running each through a function and returning the result to the next run of the function',()=>{
-    expect(reduceRight([1,2],function(a,b){return a+b},2)).toEqual(5)
+    expect(reduceRight([1,2],(a,b)=>{return a+b},2)).toEqual(5)
   })
   it('reduces a collection to a value by running each through a function and returning the result to the next run of the function',()=>{
-    expect(reduceRight([4,2],function(a,b){return a/b},8)).toEqual(1)
+    expect(reduceRight([4,2],(a,b)=>{return a/b},8)).toEqual(1)
   })
 
 })
--- spec/reduce_spec.js
+++ spec/reduce_spec.js
@@ -2,13 +2,13 @@ const reduce = require('../src/reduce.js')
 
 describe('concat',()=> {
   it('reduces a collection to a value by running each through a function and returning the result to the next run of the function',()=>{
-    expect(reduce([1,2],function(a,b){return a+b})).toEqual(3)
+    expect(reduce([1,2],(a,b)=>{return a+b})).toEqual(3)
   })
   it('reduces a collection to a value by running each through a function and returning the result to the next run of the function',()=>{
-    expect(reduce([1,2],function(a,b){return a+b},2)).toEqual(5)
+    expect(reduce([1,2],(a,b)=>{return a+b},2)).toEqual(5)
   })
   it('reduces a collection to a value by running each through a function and returning the result to the next run of the function',()=>{
-    expect(reduce([4,2],function(a,b){return a/b},8)).toEqual(1)
+    expect(reduce([4,2],(a,b)=>{return a/b},8)).toEqual(1)
   })
 
 })
